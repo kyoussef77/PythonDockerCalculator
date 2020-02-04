0dfe0b4ef96e3484ceb3eae8b6f3c04098329bbb
Kamal Youssef <43768655+kyoussef77@users.noreply.github.com>
2020-02-04 15:21:21 -0500

    Feature: Added Travis CI build file

--- Tests/test_MathOperations.py
+++ Tests/test_MathOperations.py
@@ -11,5 +11,9 @@ class MyTestCase(unittest.TestCase):
     def test_MathOperations_Subtraction(self):
         self.assertEqual(-1, Subtraction.difference(1, 2))
 
+    def test_MathOperations_Sum_list(self):
+        numlist = [1,2,3]
+        self.assertEqual(6, Addition.sum(numlist))
+
 if __name__ == '__main__':
     unittest.main()
